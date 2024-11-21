ea715ae8ccd5fd6fe243c2512dcb3aa92304becc
farrelathalla <farrelxag@gmail.com>
2024-11-21 13:45:03 +0700

    feat: add routing

--- 3-form-app/src/App.js
+++ 3-form-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FormPage from './pages/FormPage';
+import ReviewPage from './pages/ReviewPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{fontFamily: 'Helvetica Neue'}}>
+    <Router>
+      <div className="App">
+        {/* Define the routing pages */}
+        <Routes>
+          {/* Form Page at the root */}
+          <Route path="/" element={<FormPage />} />
+
+          {/* Review Page */}
+          <Route path="/review" element={<ReviewPage />} />
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
