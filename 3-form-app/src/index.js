6fca597393088350236167bca345275809789a1c
farrelathalla <farrelxag@gmail.com>
2024-11-21 13:45:36 +0700

    feat: add routing

--- 3-form-app/src/index.js
+++ 3-form-app/src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'; // Import Provider
+import { configureStore } from '@reduxjs/toolkit'; // Import configureStore
+import formReducer from './features/formSlice'; // Import your formSlice
+
+// Create the Redux store
+const store = configureStore({
+  reducer: {
+    form: formReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Wrap App with Provider and pass the store */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Performance logging (optional)
 reportWebVitals();
