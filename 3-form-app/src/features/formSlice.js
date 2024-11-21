be8ac5c08cbf8d06c81e9aeadb9545568fdab12e
farrelathalla <farrelxag@gmail.com>
2024-11-21 13:40:51 +0700

    feat: add form slice

---
+++ 3-form-app/src/features/formSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    formData: {},
+};
+
+const formSlice = createSlice({
+    name: 'form',
+    initialState,
+    reducers: {
+        saveFormData: (state, action) => {
+            state.formData = action.payload;
+        },
+    },
+});
+
+export const { saveFormData } = formSlice.actions;
+export default formSlice.reducer;
